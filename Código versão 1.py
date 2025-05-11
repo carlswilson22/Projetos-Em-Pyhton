fd40adec9f65f3203a5733bf1a51d8d91c853997
Carlos Wilson <carlos.wilsong@sempreceub.com>
2025-05-11 20:41:35 -0300

    Create Código versão 1.py

---
+++ Código versão 1.py
@@ -0,0 +1,16 @@
+# Calculadora Simples
+
+num1 = float(input("Digite o primeiro número: "))
+op = input("Digite a operação (+, -, *, /): ")
+num2 = float(input("Digite o segundo número: "))
+
+if op == '+':
+    print("Resultado:", num1 + num2)
+elif op == '-':
+    print("Resultado:", num1 - num2)
+elif op == '*':
+    print("Resultado:", num1 * num2)
+elif op == '/':
+    print("Resultado:", num1 / num2)
+else:
+    print("Operação inválida")
